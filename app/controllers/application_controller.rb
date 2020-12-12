c4296a9bae638311a4b85b8440d3c18c454348f5
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-12-12 15:28:25 +0900

    トップページの画面変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_paramaters, if: :devise_controller?
   private
   def configure_permitted_paramaters
-    devise_paramater_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
