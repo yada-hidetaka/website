d35924cdb27274f72718ce03db88696da4cfc09d
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-12-17 18:30:58 +0900

    update挙動設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
 end
