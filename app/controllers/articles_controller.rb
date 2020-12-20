4cd2b57e7906ef1e669cf88b56a5b179039380ea
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-12-20 23:41:40 +0900

    controller設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,21 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.new
   end
 
   def new
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def edit
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
   end
 end
