4354b5df6b52687696fa14beac2b74330d8b1579
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-12-20 23:12:03 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/edit'
   devise_for :users
   root to: "home#index"
   resources :users
+  resources :articles
 end
