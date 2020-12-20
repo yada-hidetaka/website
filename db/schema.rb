6f4ca9926bf2b0781964a457e7a3a008908e067c
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-12-20 23:10:38 +0900

    controller作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_095443) do
+ActiveRecord::Schema.define(version: 2020_12_20_140828) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
